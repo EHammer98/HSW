0cd7dcd19a3cec7bce25f74cf0a17ab4a7fe4e90
EHammer98 <elwinhammer@gmail.com>
2017-01-08 22:59:09 +0100

    Menu
    Bug fixes and added 2 working items (battery info & system info)

--- HSW.ino
+++ HSW.ino
@@ -36,8 +36,6 @@ extern uint8_t bootScreen[];
 extern uint8_t TEST01[];
 extern uint8_t cellphone[];
 extern uint8_t cellphoneICO[];
-extern uint8_t message[];
-extern uint8_t messageICO[];
 extern uint8_t BLEICO[];
 extern uint8_t menuItem1[];
 extern uint8_t menuItem2[];
@@ -77,6 +75,8 @@ char TimeC2[4];
 char DateC[11];
 char callC[12];
 char messageC[12];
+char voltC[5];
+char accuC[4];
 String TimeB = "";
 String TimeB2 = "";
 String DateB = "";
@@ -89,8 +89,11 @@ byte BOOT = 1;
 SoftwareSerial BLEserial(10, 11); // RX, TX
 
 //Menu
-int Menu = 0; //Menu active
-int selectedItem = 1; //Menu item selected
+int menuStat = 0; //Menu active
+int selectedItem = 0; //Menu item selected
+int batteryItem = 0;
+int systemItem = 0;
+int timeItem = 0;
 
 void drawBOOT(){  
   u8g.drawBitmapP( 0, 0, 16, 64, bootScreen);
@@ -113,10 +116,7 @@ void setup() {
      drawBOOT();
   } while( u8g.nextPage() );
     delay(3000);
-    
-  Serial.begin(9600);
 BLEserial.begin(9600);
-Serial.print("HammerIT | SmartWatch | Firmware v.0.0.1");
 pinMode(Vibe, OUTPUT);
 pinMode(BLE, OUTPUT);
   
@@ -142,12 +142,10 @@ if (currentMillis - previousMillis >= interval) {
 
     if (digitalRead(buttonPin1) == LOW) {
       buttonPushCounter1++;
-Serial.println(buttonPushCounter1);
  }
 
   if (digitalRead(buttonPin2) == LOW) {
       buttonPushCounter2++;
-Serial.println(buttonPushCounter2);
  }
 
  if (buttonPushCounter1 == 1){
@@ -155,24 +153,30 @@ Serial.println(buttonPushCounter2);
  iMissedMSG = 0;
  BLEcon = 0;
  }
- 
+
+ if (menuStat == 0){
  if (buttonPushCounter1 > 0){
   if (buttonPushCounter1 == 1){
   buttonPushCounter1=0;
   }
  }
+ }
+
+
 
   if (buttonPushCounter2 == 1){
-Menu = 1;
+menuStat = 1;
  }
- 
+
+ if (menuStat == 0){
  if (buttonPushCounter2 > 0){
   if (buttonPushCounter2 == 1){
   buttonPushCounter2=0;
   }
  }
+ }
 
-if (Menu == 0){
+if (menuStat == 0){
    if (incommingCall == 0 and inMessage == 0){
  u8g.firstPage();  
   do {
@@ -200,182 +204,104 @@ if (Menu == 0){
   delay(100);
    }
 
-    if (Menu == 1){
-      
- u8g.firstPage();  
-  do {
-    drawMenu1();
-  } while( u8g.nextPage() );
-  delay(100);
-
-   if (digitalRead(buttonPin1) == LOW) {
-      buttonPushCounter1++;
-Serial.println(buttonPushCounter1);
- }
-
-  if (digitalRead(buttonPin2) == LOW) {
-      buttonPushCounter2++;
-Serial.println(buttonPushCounter2);
- }
-
+    if (menuStat == 1){
+   
  if (buttonPushCounter1 == 1){
   selectedItem = 1;
-  u8g.firstPage();  
+ }
+  if (buttonPushCounter1 == 2){
+  selectedItem = 2;
+ }
+  if (buttonPushCounter1 == 3){
+  selectedItem = 3;
+ }
+  if (buttonPushCounter1 == 4){
+  selectedItem = 4;
+ }
+
+ if (buttonPushCounter1 > 3){
+  buttonPushCounter1=0;
+  }
+
+ if ((batteryItem == 0) && (systemItem == 0)){
+if (selectedItem == 1){
+   u8g.firstPage();  
   do {
     drawMenu1();
   } while( u8g.nextPage() );
   delay(100);
- }
-
- if (buttonPushCounter1 == 2){
-    selectedItem = 2;
-  u8g.firstPage();  
+}
+if (selectedItem == 2){
+   u8g.firstPage();  
   do {
     drawMenu2();
   } while( u8g.nextPage() );
   delay(100);
- }
-
- if (buttonPushCounter1 == 3){
-    selectedItem = 3;
-  u8g.firstPage();  
+}
+if (selectedItem == 3){
+   u8g.firstPage();  
   do {
     drawMenu3();
   } while( u8g.nextPage() );
   delay(100);
- }
-
- if (buttonPushCounter1 == 4){
-    selectedItem = 4;
-  u8g.firstPage();  
+}
+if (selectedItem == 4){
+   u8g.firstPage();  
   do {
     drawMenu4();
   } while( u8g.nextPage() );
   delay(100);
- }
- 
- if (buttonPushCounter1 > 0){
-  if (buttonPushCounter1 == 4){
-  buttonPushCounter1=0;
+    }
   }
- }
 
+ 
+ 
   if (buttonPushCounter2 == 1){
-  if (selectedItem == 1){
-    
+  if (selectedItem == 1){    
+    batteryItem = 1;
   }
-
   if (selectedItem == 2){
-    
+    timeItem = 1;
   }
-
   if (selectedItem == 3){
-    
+    systemItem = 1;
   }
-
   if (selectedItem == 4){
-    Menu = 0;
+    selectedItem = 0;
+    menuStat = 0;
+    drawTime();
   }
  }
- 
+  
+  
  if (buttonPushCounter2 > 0){
   if (buttonPushCounter2 == 1){
   buttonPushCounter2=0;
   }
  }
 
- 
-   }
-
-
-if(Serial.available()){
-  while(Serial.available())
-    {
-      char inChar = (char)Serial.read(); //read the input
-      inputString += inChar;        //make a string of the characters coming on serial
+if (batteryItem == 1){
+   u8g.firstPage();  
+  do {
+    drawBatteryItem();
+  } while( u8g.nextPage() );
+  delay(100);
+}
+if (systemItem == 1){
+   u8g.firstPage();  
+  do {
+    drawSystemItem();
+  } while( u8g.nextPage() );
+  delay(100);
+}
+if (timeItem == 1){
+   u8g.firstPage();  
+  do {
+    drawTimeItem();
+  } while( u8g.nextPage() );
+  delay(100);
     }
-    Serial.println(inputString);
-    while (Serial.available() > 0)  
-    { junk = Serial.read() ; }      // clear the serial buffer
-
-
-if (inputString ==  "VibeON"){
-    digitalWrite(Vibe, HIGH);
-    Serial.print("OK | Vibe ON");
-    vibeStat = 1;
-  }
-
-  if (inputString == "VibeOFF"){
-    digitalWrite(Vibe, LOW);
-     Serial.print("OK | Vibe OFF");
-     vibeStat = 0;
-  }
-
-  if (inputString.startsWith("Time:")){
-    inputString.remove(0,5);
-    inputString.remove(2,1);
-    inputStringB = inputString;
-    inputStringB.remove(2,2);
-    Serial.print(inputStringB);
-    inputString.remove(0,2);
-    Serial.print(inputString);
-    iHour = inputStringB.substring(0, 2).toInt();
-    iMinutes = inputString.substring(0, 2).toInt();
-    Serial.print(iHour + dots + iMinutes);
-  }
-
-
-  if (inputString.startsWith("Date:")){
-    inputString.remove(0,5);
-    inputString.remove(2,1);
-    inputStringDay = inputString;
-    inputStringDay.remove(2,5);
-    Serial.print(inputStringDay);
-    inputString.remove(0,2);
-    inputStringMonth = inputString;
-    inputStringMonth.remove(2,3);
-    Serial.print(inputStringMonth);
-    inputString.remove(0,3);
-    inputStringYear = inputString;
-    Serial.print(inputStringYear);
-    iDay = inputStringDay.substring(0, 2).toInt();
-    iMonth = inputStringMonth.substring(0, 2).toInt();
-    iYear = inputStringYear.substring(0, 2).toInt();
-        Serial.print(iDay + stripe + iMonth + stripe + iYear);
-  }
-
-  if (inputString.startsWith("Call:")){
-          iSecond2 = 0;
-    incommingCall = 1;
-    inputString.remove(0,5);
-    Serial.print(inputString);
-    iCall = iZero + inputString.substring(0, 12).toInt();
-    Serial.print(iCall);
-    delay(50);
-  }
-
-  if (inputString.startsWith("GetVolt")){
-       volt= analogRead(voltPin1);
-  String voltage = String(volt* (3.7 * 11 / 1023) * 10 );
-  String accu = String(volt* (3.7 * 11 / 1023) / 3.7 * 100);
-    Serial.println(voltage);
-    Serial.println(accu + "%");
-    
-  }
-
-
-if (inputString.startsWith("MSG:")){
-      iSecond2 = 0;
-    inMessage = 1;
-    Serial.print(inputString);
-    inputString.remove(0,4);
-    iMessage = inputString;
-    Serial.print(iMessage);
-  }
-  
-inputString = "";
- }
-
+    }
  if(BLEserial.available()){
   while(BLEserial.available())
     {
@@ -389,7 +315,7 @@ inputString = "";
 
 if (inputString2 ==  "VibeON"){
     digitalWrite(Vibe, HIGH);
-    Serial.print("OK | Vibe ON");
+    BLEserial.print("OK | Vibe ON");
     vibeStat = 1;
   }
 
@@ -407,7 +333,7 @@ if (inputString2 ==  "VibeON"){
     inputString2.remove(0,2);
     iHour = inputStringB.substring(0, 2).toInt();
     iMinutes = inputString2.substring(0, 2).toInt();
-    Serial.print(iHour + dots + iMinutes);
+    BLEserial.print(iHour + dots + iMinutes);
   }
 
 
@@ -424,7 +350,7 @@ if (inputString2 ==  "VibeON"){
     iDay = inputStringDay.substring(0, 2).toInt();
     iMonth = inputStringMonth.substring(0, 2).toInt();
     iYear = inputStringYear.substring(0, 2).toInt();
-    Serial.print(iDay + stripe + iMonth + stripe + iYear);
+    BLEserial.print(iDay + stripe + iMonth + stripe + iYear);
   }
 
    if (inputString2.startsWith("Call:")){
@@ -433,16 +359,16 @@ if (inputString2 ==  "VibeON"){
     Serial.print(inputString2);
     inputString2.remove(0,8);
     iCall = iZero + inputString2.substring(0, 12).toInt();
-    Serial.print(iCall);
+    BLEserial.print(iCall);
   }
 
   if (inputString2.startsWith("MSG:")){
     iSecond2 = 0;
     inMessage = 1;
-    Serial.print(inputString2);
+    BLEserial.print(inputString2);
     inputString2.remove(0,4);
     iMessage = inputString2;
-    Serial.print(iMessage);
+    BLEserial.print(iMessage);
   }
 
   if (inputString2.startsWith("BLE")){
@@ -486,7 +412,6 @@ else if (extraZeroH == 0 and extraZeroM == 0){
  DateB = Date;
  
 }
-
 void updateTime(){
    if(iSecond >= 0) {
       iSecond++; 
@@ -558,7 +483,7 @@ if (iMissedCall == 1){
 }
 
 if (iMissedMSG == 1){
-      u8g.drawBitmapP(0, 0, 16, 64, messageICO);
+     
 }
 
 if (BLEcon == 1){
@@ -588,7 +513,7 @@ void drawCall(){
 
 
 void drawMessage(){
-  u8g.drawBitmapP(0, 0, 16, 64, message);
+  
   u8g.drawFrame(5,5,121,50);
   iMessage.toCharArray(messageC, 12);
     u8g.setFont(u8g_font_freedoomr10r);
@@ -621,4 +546,54 @@ u8g.drawBitmapP(0, 0, 16, 64, menuItem3);
 }
 void drawMenu4(){
 u8g.drawBitmapP(0, 0, 16, 64, menuItem4);
+}
+
+void drawBatteryItem(){
+         volt= analogRead(voltPin1);
+  String voltage = String(volt* (3.7 * 11 / 1023) * 10 );
+  String accu = String(volt* (3.7 * 11 / 1023) / 3.7 * 100);
+    voltage.toCharArray(voltC, 5);
+    accu.toCharArray(accuC, 4);
+    u8g.setFont(u8g_font_tpssb);
+    u8g.drawStr(8, 15, "Accu Voltage:");
+    u8g.drawStr(91, 15, voltC);
+    u8g.drawStr(116, 15, "V");
+    u8g.drawStr(8, 30, "Accu inhoud:");
+       u8g.drawStr(88, 30, accuC);
+    u8g.drawStr(113, 30, "%");
+u8g.drawBox(45,43,35,16);
+u8g.drawBox(80,46,4,8); 
+  if (digitalRead(buttonPin2) == LOW) {   
+batteryItem = 0; 
+selectedItem = 1;
+ u8g.firstPage();  
+  do {
+    drawMenu1();
+  } while( u8g.nextPage() );
+  delay(100);
+}
+}
+
+void drawSystemItem(){
+    u8g.setFont(u8g_font_tpssb);
+    u8g.drawStr(8, 15, "Product:");
+    u8g.drawStr(70, 15, "HSW");
+    u8g.drawStr(8, 30, "Systeem:");
+    u8g.drawStr(70, 30, "MiniPro5V");
+     u8g.drawStr(8, 45, "Firmware:");
+      u8g.drawStr(70, 45, "0.0.2");
+      u8g.drawStr(60, 60, ";D");
+  if (digitalRead(buttonPin2) == LOW) {   
+systemItem = 0; 
+selectedItem = 2;
+ u8g.firstPage();  
+  do {
+    drawMenu3();
+  } while( u8g.nextPage() );
+  delay(100);
+}
+}
+
+void drawTimeItem(){
+   
 }
